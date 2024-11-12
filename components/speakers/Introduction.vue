e6f3041ba4fd8b9e58c78ed3c090f6047ca3c511
flora <flora_chen2021@163.com>
2024-11-12 12:42:32 +0800

    fix: i18n with @ and & error

--- components/speakers/Introduction.vue
+++ components/speakers/Introduction.vue
@@ -2,24 +2,20 @@
 import type { Speaker } from '~/lib/model';
 
 const props = defineProps<{
-    speakers: Speaker
+  speakers: Speaker
 }>()
 
 </script>
 
 <template>
   <UCard>
-    <template #header>
-      {{ props.speakers.bio.details.title }}
-    </template>
-
     <div class="flex flex-col w-full">
       <UAvatar :src="props.speakers.photo" icon="i-heroicons-photo" class="w-24 h-24 rounded-full mx-auto" />
       <div class="font-bold text-center text-xl py-2">
         {{ props.speakers.name }}
       </div>
       <div>
-        {{ props.speakers.bio.details.description }}
+        {{ props.speakers.bio.details.title }}
       </div>
     </div>
 
--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,7 @@ import type { Speaker } from "./model";
 
 export const keynoteSpeaker: Array<Speaker> = [
     {
-        name: "Professor LEE, Chi-Kin John",
+        name: "server.Keynote Speakers.Professor LEE, Chi-Kin John.name",
         kind: "Keynote Speakers",
         photo: "./assets/avatars/Chi-Kin John.png",
         activateDate: new Date("2025-05-09"),
@@ -15,46 +15,46 @@ export const keynoteSpeaker: Array<Speaker> = [
         }
     },
     {
-        name: "Prof. Ren Youqun",
+        name: "server.Keynote Speakers.Ren Youqun.name",
         kind: "Keynote Speakers",
         photo: "./assets/avatars/Ren Youqun.png",
         activateDate: new Date("2025-05-09"),
         bio: {
             details: {
-                description: "server.Keynote Speakers.Prof. Ren Youqun.description",
-                title: "server.Keynote Speakers.Prof. Ren Youqun.title",
+                description: "server.Keynote Speakers.Ren Youqun.description",
+                title: "server.Keynote Speakers.Ren Youqun.title",
                 link: undefined
             }
         }
     },
     {
-        name: "Dr. Chris Dede",
+        name: "server.Keynote Speakers.Chris Dede.name",
         kind: "Keynote Speakers",
         photo: "./assets/avatars/Chris Dede.png",
         activateDate: new Date("2025-05-09"),
         bio: {
             details: {
-                description: "server.Keynote Speakers.Dr. Chris Dede.description",
-                title: "server.Keynote Speakers.Dr. Chris Dede.title",
+                description: "server.Keynote Speakers.Chris Dede.description",
+                title: "server.Keynote Speakers.Chris Dede.title",
                 link: undefined
             }
         }
     },
     {
-        name: "Dr. Shijin Wang",
+        name: "server.Keynote Speakers.Shijin Wang.name",
         kind: "Keynote Speakers",
         photo: "./assets/avatars/Shijin Wang.png",
         activateDate: new Date("2025-05-09"),
         bio: {
             details: {
-                description: "server.Keynote Speakers.Dr. Shijin Wang.description",
-                title: "server.Keynote Speakers.Dr. Shijin Wang.title",
+                description: "server.Keynote Speakers.Shijin Wang.description",
+                title: "server.Keynote Speakers.Shijin Wang.title",
                 link: undefined
             }
         }
     },
     {
-        name: "Professor Minjuan WANG",
+        name: "server.Keynote Speakers.Professor Minjuan WANG.name",
         kind: "Keynote Speakers",
         photo: "./assets/avatars/Minjuan WANG.png",
         activateDate: new Date("2025-05-09"),
@@ -67,20 +67,20 @@ export const keynoteSpeaker: Array<Speaker> = [
         }
     },
     {
-        name: "Dr. Yew-Soon Ong",
+        name: "server.Keynote Speakers.Yew-Soon Ong.name",
         kind: "Keynote Speakers",
         photo: "./assets/avatars/Yew-Soon Ong.png",
         activateDate: new Date("2025-05-09"),
         bio: {
             details: {
-                description: "server.Keynote Speakers.Dr. Yew-Soon Ong.description",
-                title: "server.Keynote Speakers.Dr. Yew-Soon Ong.title",
+                description: "server.Keynote Speakers.Yew-Soon Ong.description",
+                title: "server.Keynote Speakers.Yew-Soon Ong.title",
                 link: undefined
             }
         }
     },
     {
-        name: "Paul Kim",
+        name: "server.Keynote Speakers.Paul Kim.name",
         kind: "Keynote Speakers",
         photo: "./assets/avatars/Paul Kim.png",
         activateDate: new Date("2025-05-09"),
@@ -93,16 +93,16 @@ export const keynoteSpeaker: Array<Speaker> = [
         }
     },
     {
-        name: "Miss. Anny Chan",
+        name: "server.Keynote Speakers.Anny Chan.name",
         kind: "Keynote Speakers",
         photo: "./assets/avatars/Anny Chan.png",
         activateDate: new Date("2025-05-09"),
         bio: {
             details: {
-                description: "server.Keynote Speakers.Miss. Anny ChanOng.description",
-                title: "server.Keynote Speakers.Miss. Anny Chan.title",
+                description: "server.Keynote Speakers.Anny Chan.description",
+                title: "server.Keynote Speakers.Anny Chan.title",
                 link: undefined
             }
         }
     },
-] 
+];
