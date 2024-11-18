20d0f5c1d95759c5549c0ed87006fc183eae64aa
virgil <virgiling7@gmail.com>
2024-11-18 17:49:28 +0800

    fix: verify params when requesting for file

--- server/api/poster/getCallByID.get.ts
+++ server/api/poster/getCallByID.get.ts
@@ -6,13 +6,20 @@ export default defineEventHandler(async (event) => {
     const t = await useTranslation(event)
 
     const filePath = t(query.path as string)
-
+    const regex = /^\.\/assets\/poster\/([a-zA-Z0-9_-]+)\.pdf$/;
+    const match = filePath.match(regex) && filePath.split('/').length === 4;
 
     try {
-        const pdf = fs.readFileSync(filePath)
-        event.node.res.setHeader('Content-Type', 'application/pdf')
-        event.node.res.write(Buffer.from(pdf))
-        event.node.res.end()
+        if (match) {
+            const pdf = fs.readFileSync(filePath)
+            event.node.res.setHeader('Content-Type', 'application/pdf')
+            event.node.res.write(Buffer.from(pdf))
+            event.node.res.end()
+        }
+        else {
+            event.node.res.statusCode = 400;
+            event.node.res.end('Invalid PDF File Path');
+        }
     } catch (err) {
         event.node.res.statusCode = 404
     }
--- server/api/speaker/photo.get.ts
+++ server/api/speaker/photo.get.ts
@@ -2,8 +2,20 @@ import fs from 'node:fs'
 
 export default defineEventHandler(async (event: any) => {
     const query = getQuery(event)
-    const image = fs.readFileSync(query.photo as string)
-    event.node.res.setHeader('Content-Type', 'image/png')
-    event.node.res.write(Buffer.from(image))
-    event.node.res.end()
+    const imagePath = query.photo as string
+
+    const regex = /^\.\/assets\/avatars\/([a-zA-Z0-9\s_-]+)\.png$/;
+    const match = imagePath.match(regex) && imagePath.split('/').length === 4;
+
+    if (match) {
+        const image = fs.readFileSync(imagePath)
+        event.node.res.setHeader('Content-Type', 'image/png')
+        event.node.res.write(Buffer.from(image))
+        event.node.res.end()
+    } else {
+        event.node.res.statusCode = 400;
+        event.node.res.end('Invalid Avatar Path');
+    }
+
+
 })
