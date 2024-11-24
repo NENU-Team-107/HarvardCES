645e590385413af58c934013217f840c3e5ea891
flora <flora_chen2021@163.com>
2024-11-25 00:13:18 +0800

    footer display

--- components/common/AppFooter.vue
+++ components/common/AppFooter.vue
@@ -42,64 +42,87 @@ const media = ref<MediaItemType[]>([
   },
 ])
 
+const isHover = ref<boolean[]>([false, false])
+
 </script>
 
 <template>
-  <div class="min-w-full min-h-28 bg-green-950/90 dark:bg-green-950 text-white w-full">
-    <div class="text-xl font-bold text-center h-10"></div>
-    <div class="text-xl font-bold text-center md:hidden relative">{{ $t(footerTitle) }}</div>
-    <div class="flex md:flex-row flex-col items-center justify-center gap-3 w-full">
-      <div class="flex justify-center items-center mx-10">
-        <div>
-          <div class="items-center justify-center mb-2 max-w-md max-h-28">
-            <UButton icon="i-fa6-solid-heart" padding="2xs" gap="xs" color="sky" variant="soft"
-              :ui="{ rounded: 'rounded-full', font: 'font-bold' }" block :trailing="true" to="" target="_blank">
-              <span class="text-lg">Sponsor Ship</span>
-              <!-- <template #trailing>
-                <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
-              </template> -->
-            </UButton>
-          </div>
-          <NuxtImg src="img/logo/UNESCOLogo-single.png" sizes="320" />
+  <div class="min-w-full min-h-28 bg-green-950/90 dark:bg-green-950 text-white w-full py-6">
+    <div class="text-xl font-bold text-center relative leading-10 pb-2">
+      {{ $t(footerTitle) }}
+    </div>
+
+    <div class="grid md:grid-cols-3 md:grid-rows-1 grid-cols-1 grid-rows-3 w-full">
+
+      <div class="grid grid-cols-1 grid-rows-3 justify-center items-center mx-10">
+        <div class="flex items-center justify-center max-w-md max-h-28">
+          <UButton icon="i-fa6-solid-heart" padding="2xs" gap="xs" color="sky" variant="soft"
+            :ui="{ rounded: 'rounded-full', font: 'font-bold' }" class="w-1/2" block :trailing="true" to=""
+            target="_blank">
+            <span class="text-lg">Sponsor Ship</span>
+          </UButton>
+        </div>
+        <div class="w-full justify-center items-center flex row-span-2">
+          <NuxtImg src="img/logo/UNESCOLogo-single.png" />
         </div>
       </div>
-      <div class="max-w-screen-md">
-        <div class="text-xl font-bold text-center hidden md:-top-10 md:block relative">{{ $t(footerTitle) }}</div>
-        <!-- <NuxtImg src="img/logo/HostLogo_small.jpg" sizes="300" class="justify-self-center" /> -->
-        <div class="grid grid-cols-2 gap-4 text-center ml-5">
+
+      <div class="max-w-screen-md py-6">
+        <div class="grid md:grid-cols-2 gap-4 md:grid-rows-3 grid-rows-1 grid-cols-1 text-center ml-5">
           <!-- TODO 这里改为联系邮箱等信息 -->
-          <div>
-            <div class="grid grid-rows-2 gap-2">
-              <div>{{ $t("Harvard") }}</div>
-              <div class="flex justify-center items-center">
-                <UButton icon="i-material-symbols-public" size="lg" color="rose" square variant="soft" class="mr-2"
-                  to="https://www.hgseces.org/" />
-                <UButton icon="i-material-symbols-mail-rounded" size="lg" color="rose" square variant="soft"
-                  class="ml-2" to="mailto:general@hgseces.org" />
-              </div>
-            </div>
+
+          <div class="row-span-2">{{ $t("Harvard") }}</div>
+
+          <div class="md:hidden flex justify-center items-center" @mouseover="isHover[0] = true"
+            @mouseleave="isHover[0] = false">
+            <UButton icon="i-material-symbols-public" size="lg" color="rose" square variant="soft"
+              :class="isHover[0] ? 'mr-2 bg-white/60' : 'mr-2 bg-white'" :ui="{ rounded: 'rounded-full', }"
+              to="https://www.hgseces.org/" />
+            <UButton icon="i-material-symbols-mail-rounded" size="lg" color="rose" square variant="soft"
+              :class="isHover[0] ? 'ml-2 bg-white/60' : 'ml-2 bg-white'" :ui="{ rounded: 'rounded-full' }"
+              to="mailto:general@hgseces.org" />
           </div>
-          <div class="grid grid-rows-2 gap-2">
-            <div> {{ $t("GIET") }}</div>
-            <div class="flex justify-center items-center">
-              <UButton icon="i-material-symbols-public" size="lg" color="primary" square variant="soft" class="mr-2"
-                to="https://www.eduhk.hk/en/" />
-              <UButton icon="i-material-symbols-mail-rounded" size="lg" color="primary" square variant="soft"
-                class="ml-2" to="mailto:GIETfuture@eduhk.hk" />
-            </div>
+
+          <div class="row-span-2"> {{ $t("GIET") }}</div>
+
+          <div class="hidden md:flex justify-center items-center" @mouseover="isHover[0] = true"
+            @mouseleave="isHover[0] = false">
+            <UButton icon="i-material-symbols-public" size="lg" color="rose" square variant="soft"
+              :class="isHover[0] ? 'mr-2 bg-white/60' : 'mr-2 bg-white'" :ui="{ rounded: 'rounded-full', }"
+              to="https://www.hgseces.org/" />
+            <UButton icon="i-material-symbols-mail-rounded" size="lg" color="rose" square variant="soft"
+              :class="isHover[0] ? 'ml-2 bg-white/60' : 'ml-2 bg-white'" :ui="{ rounded: 'rounded-full' }"
+              to="mailto:general@hgseces.org" />
+          </div>
+
+          <div class="flex justify-center items-center" @mouseover="isHover[1] = true" @mouseleave="isHover[1] = false">
+            <UButton icon="i-material-symbols-public" size="lg" color="primary" square variant="soft"
+              :class="isHover[1] ? 'mr-2 bg-white/60' : 'mr-2 bg-white'" :ui="{ rounded: 'rounded-full', }"
+              to="https://www.eduhk.hk/en/" />
+            <UButton icon="i-material-symbols-mail-rounded" size="lg" color="primary" square variant="soft"
+              :class="isHover[1] ? 'ml-2 bg-white/60' : 'ml-2 bg-white'" :ui="{ rounded: 'rounded-full' }"
+              to="mailto:GIETfuture@eduhk.hk" />
           </div>
         </div>
       </div>
+
       <div class="flex flex-col">
         <div class="flex flex-col justify-center items-center">
-          <div class="flex flex-row">
-            <div v-for="item in media" class="flex flex-row ml-5">
-              <div class="flex flex-col justify-center items-center text-justify">
-                <span>{{ $t("Media." + item.label + ".Intro") }}</span>
-                <NuxtImg v-if="item.label !== 'Email'" :src="item.image" class="w-20 h-20"></NuxtImg>
-                <span> {{ $t("Media." + item.label + ".Details") }}</span>
+          <div class="grid grid-rows-1 grid-cols-2 ">
+            <div v-for="item in media" class="ml-5">
+              <div class="grid grid-rows-3 grid-cols-1 text-justify">
+                <div class="row-span-2">
+                  <span class="flex justify-center items-center py-4">
+                    {{ $t("Media." + item.label + ".Intro") }}
+                  </span>
+                  <div class="flex justify-center items-center">
+                    <NuxtImg v-if="item.label !== 'Email'" :src="item.image" class="w-20 h-20"></NuxtImg>
+                  </div>
+                </div>
+                <span class="flex justify-center items-center text-center">
+                  {{ $t("Media." + item.label + ".Details") }}
+                </span>
               </div>
-              <!--  -->
             </div>
           </div>
           <!-- <div class="my-2">
