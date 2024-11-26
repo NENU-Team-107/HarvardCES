6868ca8d2301e69c4b67568c06b7f56ccf3fc5dc
virgil <virgiling7@gmail.com>
2024-11-26 11:16:42 +0800

    feat: add bg color

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,11 @@
   <div class="w-full min-h-screen flex flex-col relative dark:bg-black">
     <!-- 背景图片 -->
     <div
-      class="absolute top-0 left-0 inset-0 dark:bg-none bg-letter-paper bg-no-repeat bg-center bg-cover blur-sm opacity-80 bg-fixed dark:opacity-95 " />
+      class="absolute top-0 left-0 inset-0 dark:bg-none bg-letter-paper bg-no-repeat bg-center bg-cover blur-sm opacity-80 bg-fixed dark:opacity-95" />
     <div class="w-full flex flex-col justify-center relative z-10">
       <AppHeader />
       <!-- 内容 -->
-      <div class="h-fit min-h-screen flex flex-col items-center w-full">
+      <div class="h-fit min-h-screen flex flex-col items-center w-full bg-yellow-100/10">
         <slot />
       </div>
 
