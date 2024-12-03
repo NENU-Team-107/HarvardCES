d52288307f580518e7d47ad0ec05ed4dcc3953c2
virgil <virgiling7@gmail.com>
2024-12-03 09:59:04 +0800

    fix: agenda colunms line and agenda note

--- pages/symposium/time.vue
+++ pages/symposium/time.vue
@@ -14,12 +14,18 @@ const AgendaImage = computed(() => {
             class="md:flex hidden flex-col px-8 md:px-16 bg-white/50 py-4  h-full w-full shadow-2xl  border-1 rounded-lg self-center relative">
             <div class="w-full">
                 <NuxtImg :src="AgendaImage" loading="lazy" class="w-full object-cover" />
+                <div class="text-center my-2">
+                    <i>{{ $t("Agenda Note") }}</i>
+                </div>
             </div>
         </div>
 
         <div class="md:hidden">
             <div class="w-full">
                 <NuxtImg :src="AgendaImage" loading="lazy" class="w-full object-cover" />
+                <div class="text-center text-xs my-2">
+                    <i>{{ $t("Agenda Note") }}</i>
+                </div>
             </div>
         </div>
     </div>
