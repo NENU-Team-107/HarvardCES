5ce2aca97a6ace4bbc1d8460799b979899abdd46
virgil <virgiling7@gmail.com>
2025-01-17 19:37:16 +0800

    fix: change cover image

--- lib/data.ts
+++ lib/data.ts
@@ -575,12 +575,12 @@ export const routerArray: Array<RouterItem> = [
                 path: "/contact/committee"
             },
             {
-                name: 'Harvard CES',
-                path: '/contact/ces'
-            },
-            {
                 name: "EduHK",
                 path: '/contact/eduhk'
+            },
+            {
+                name: 'Harvard CES',
+                path: '/contact/ces'
             },
             // {
             //     name: "New MA Program",
