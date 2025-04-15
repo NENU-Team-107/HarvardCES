3a0d62ba8fd85d0c91aee879b65deca8d0a9ed94
Flora Chen <flora_chen2021@163.com>
2025-04-15 11:42:53 +0800

    remove program overview

--- pages/symposium/time.vue
+++ pages/symposium/time.vue
@@ -21,7 +21,7 @@ const adjustIframe = () => {
 <template>
   <div class="max-w-6xl mx-10 my-5 pt-24 w-full h-full bg-white/50 shadow-2xl ">
 
-    <div :class="['flex justify-center items-center text-5xl py-10', $t('special font'),]">
+    <!-- <div :class="['flex justify-center items-center text-5xl py-10', $t('special font'),]">
       <div class="h-0.5 w-20 bg-black"></div>
       <h1 class="mx-4">{{ $t("Symposium Handbook") }} </h1>
       <div class="h-0.5 w-20 bg-black"></div>
@@ -30,7 +30,7 @@ const adjustIframe = () => {
     <div class="min-w-full min-h-max flex justify-center items-center">
       <iframe frameborder="0" scrolling="auto" id="bi_iframe" :style="{ width: iframeWidth, height: iframeHeight }"
         @load="adjustIframe" :src="fileURL"></iframe>
-    </div>
+    </div> -->
 
     <div class="md:flex hidden flex-col px-8 md:px-16 py-4  h-full w-full  border-1 rounded-lg self-center">
       <div class="flex justify-center items-center text-4xl my-4 font-bold">
